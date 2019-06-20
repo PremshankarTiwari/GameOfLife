3e09f15acde6e5f140830cf8832547af81e7ac31
Ojha <tiwaripremshankar8@gmail.com>
2019-06-20 18:16:25 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,192 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class GameBoard extends Component {
+  title = "Conway's Game of Life";
+  state = {
+    board: this.makeArray(10, 10),
+    steps: 0
+  };
+  interval;
+  delay = 1000;
+  colStyles = {
+    borderColor: "#ccc",
+    borderWidth: 1,
+    borderStyle: "solid",
+    height: 20,
+    width: 20,
+    margin: 2
+  };
+  contStyles = {
+    width: 400,
+    padding: 5
+  };
+  controlStyle = {
+    height: 30,
+    fontSize: 12,
+    fontWeight: "bold",
+    marginLeft: 10,
+    marginRight: 10
+  };
+  render() {
+    let board = this.state.board,
+      rows = board.length,
+      cols = board[0].length,
+      rowsArr = [];
+    for (let i = 0; i < rows; i++) {
+      let colsArr = [];
+      for (let j = 0; j < cols; j++) {
+        let exp = board[i][j] ? (
+          <div
+            key={i + "-" + j}
+            style={this.colStyles}
+            className="col-sm-1 filled"
+            onClick={this.toggleCell.bind(this, i, j, 1)}
+          />
+        ) : (
+          <div
+            key={i + "-" + j}
+            style={this.colStyles}
+            className="col-sm-1 blank"
+            onClick={this.toggleCell.bind(this, i, j, 0)}
+          />
+        );
+        colsArr.push(exp);
+      }
+      rowsArr.push(
+        <div key={i} className="row">
+          {colsArr}
+        </div>
+      );
+    }
+    return (
+      <div>
+        <header>{this.title}</header>
+        <div id="content">
+          <p>
+            The Game of Life, also known simply as Life, is a cellular automaton
+            devised by the British mathematician John Horton Conway in 1970. The
+            game is a zero-player game, meaning that its evolution is determined
+            by its initial state, requiring no further input. You can check the
+            rules{" "}
+            <a
+              target="blank"
+              href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life."
+            >
+              here
+            </a>
+            .
+          </p>
+          <h2>Play Game below</h2>
+          <div style={this.contStyles} align="center" className="container">
+            {rowsArr}
+          </div>
+          <div id="ctrl-prt">
+            <span id="steps">steps: {this.state.steps}</span>
+            <button
+              style={this.controlStyle}
+              className="btn btn-info play"
+              onClick={this.startGame.bind(this)}
+            >
+              Play
+            </button>
+            <button
+              style={this.controlStyle}
+              className="btn btn-info pause"
+              onClick={this.pauseGame.bind(this)}
+            >
+              Pause
+            </button>
+            <button
+              style={this.controlStyle}
+              className="btn btn-info step"
+              onClick={this.stepGame.bind(this)}
+            >
+              Step
+            </button>
+            <button
+              style={this.controlStyle}
+              className="btn btn-info reset"
+              onClick={this.resettGame.bind(this)}
+            >
+              Reset
+            </button>
+          </div>
+        </div>
+        <footer>Copyright - xyz</footer>
+      </div>
+    );
+  }
+
+  makeArray(h, w) {
+    var arr = [];
+    for (let i = 0; i < h; i++) {
+      arr[i] = [];
+      for (let j = 0; j < w; j++) {
+        if (j === 5) {
+          arr[i][j] = 1;
+        } else {
+          arr[i][j] = 0;
+        }
+      }
+    }
+    return arr;
+  }
+
+  countNeighbours(x, y, board) {
+    let neighbors = 0;
+
+    if (board[x - 1] && board[x - 1][y - 1] === 1) neighbors++;
+    if (board[x] && board[x][y - 1] === 1) neighbors++;
+    if (board[x + 1] && board[x + 1][y - 1] === 1) neighbors++;
+    if (board[x + 1] && board[x + 1][y] === 1) neighbors++;
+    if (board[x - 1] && board[x - 1][y] === 1) neighbors++;
+    if (board[x - 1] && board[x - 1][y + 1] === 1) neighbors++;
+    if (board[x] && board[x][y + 1] === 1) neighbors++;
+    if (board[x + 1] && board[x + 1][y + 1] === 1) neighbors++;
+
+    return neighbors;
+  }
+
+  startGame(newBoard) {
+    this.interval = setInterval(this.stepGame.bind(this), this.delay);
+  }
+
+  pauseGame() {
+    clearInterval(this.interval);
+  }
+
+  stepGame() {
+    let board = this.state.board;
+    let updatedBoard = [];
+    board.forEach((row, x) => {
+      updatedBoard[x] = [];
+      board[x].forEach((col, y) => {
+        let alive = 0,
+          count = this.countNeighbours(x, y, board);
+
+        if (col > 0) {
+          alive = count === 2 || count === 3 ? 1 : 0;
+        } else {
+          alive = count === 3 ? 1 : 0;
+        }
+
+        updatedBoard[x][y] = alive;
+      });
+    });
+    this.setState({ board: updatedBoard, steps: this.state.steps + 1 });
+  }
+
+  resettGame() {
+    this.pauseGame();
+    this.setState({ board: this.makeArray(10, 10), steps: 0 });
+  }
+
+  toggleCell(i, j, filled) {
+    //console.log(this);
+    let board = this.state.board;
+    board[i][j] = !filled;
+    this.setState({ board: board });
+  }
 }
 
-export default App;
+export default GameBoard;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
+import App from "./App";
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+configure({ adapter: new Adapter() });
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it("displays header message", () => {
+  const wrapper = shallow(<App />);
+  const welcome = <header>Conway's Game of Life</header>;
+  // expect(wrapper.contains(welcome)).toBe(true);
+  expect(wrapper.contains(welcome)).toEqual(true);
+});
+
+it("should perform play", () => {
+  const startGameSpy = jest.spyOn(App.prototype, "startGame");
+  const wrapper = shallow(<App />);
+
+  const playButtonWrapper = wrapper.find("button.play");
+  expect(playButtonWrapper.length).toBe(1);
+
+  playButtonWrapper.simulate("click");
+  expect(startGameSpy).toHaveBeenCalled();
+});
+
+it("should perform pause", () => {
+  const pauseGameSpy = jest.spyOn(App.prototype, "pauseGame");
+  const wrapper = shallow(<App />);
+
+  const pauseButtonWrapper = wrapper.find("button.pause");
+  expect(pauseButtonWrapper.length).toBe(1);
+
+  pauseButtonWrapper.simulate("click");
+  expect(pauseGameSpy).toHaveBeenCalled();
+});
+
+it("should perform step", () => {
+  const stepGameSpy = jest.spyOn(App.prototype, "stepGame");
+  const wrapper = shallow(<App />);
+
+  const stepButtonWrapper = wrapper.find("button.step");
+  expect(stepButtonWrapper.length).toBe(1);
+
+  stepButtonWrapper.simulate("click");
+  expect(stepGameSpy).toHaveBeenCalled();
+});
+
+it("should perform reset", () => {
+  const resettGameGameSpy = jest.spyOn(App.prototype, "resettGame");
+  const wrapper = shallow(<App />);
+
+  const resetButtonWrapper = wrapper.find("button.reset");
+  expect(resetButtonWrapper.length).toBe(1);
+
+  resetButtonWrapper.simulate("click");
+  expect(resettGameGameSpy).toHaveBeenCalled();
 });
